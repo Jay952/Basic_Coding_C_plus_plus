2b3f57952d69cdcb4e4da467fc7912199a53eae2
[Jay952] <[jaydeep4420@gmail.com]>
2019-12-19 23:27:49 +0530

    1D Array added

---
+++ 1D_Array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[5];
+	for(int i=0;i<5;i++)
+	{
+		cin>>arr[i];
+	
+    }
+    
+	for(int i=0;i<5;i++)
+	
+	{
+		
+	cout<<arr[i]<<endl;
+    }
+    
+	return 0;
+}
