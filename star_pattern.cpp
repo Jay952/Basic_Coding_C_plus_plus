51823e2ab8d54d06b7ac250f1d293bb12f713e2d
[Jay952] <[jaydeep4420@gmail.com]>
2019-12-19 17:36:42 +0530

    star pattern file Added

---
+++ star_pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+     int i,j;
+	for(i=0;i<5;i++)
+	{
+		for(j=0;j<=i;j++)
+		{
+		cout<<"*";
+	    }
+	 j=0;
+	 cout<<"\n";
+	
+	}
+	return 0;
+}
